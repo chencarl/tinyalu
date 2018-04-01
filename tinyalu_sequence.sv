71b138e091ffd6059f1ae94675450892ef34221a
chencarl <stander@foxmail.com>
2018-04-01 11:44:46 +0800

    Update tinyalu_sequence.sv

--- tinyalu_sequence.sv
+++ tinyalu_sequence.sv
@@ -8,11 +8,7 @@ class tinyalu_sequence extends uvm_sequence#(tinyalu_transaction);
     task body();
         tinyalu_transaction    ta_tx;
         repeat(15)  begin
-            ta_tx = tinyalu_transaction::type_id::create(.name("ta_tx"), .contxt(get_full_name()));
-            
-            start_item(ta_tx);
-            assert(ta_tx.randomize());
-            finish_item(ta_tx);
+            `uvm_do(ta_tx)
         end
     endtask
 endclass
